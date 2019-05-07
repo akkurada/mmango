6cd061780b068069e4d538d38879e8edffbeabfd
RAMAKRISHNA <nagesh.akkurada@gmail.com>
2019-05-07 23:21:18 +0530

    second

---
+++ basha/sum.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+
+{
+int a,b,c;
+printf("\n Enter vakue of A:");
+scanf("%d", &a);
+printf("Enter Value of B: ");
+scanf("%d", &b);
+
+c = a+b;
+
+printf("\n Sum:%d ", c);
+return (0) ;
+
+}
